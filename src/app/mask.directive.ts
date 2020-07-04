dc7d818287f7ab52a2e2b2612418c57cca0ec074
phani1849 <phani.guduri01@gmail.com>
2020-07-05 04:55:04 +0530

    Add ifens to my code

--- src/app/mask.directive.ts
+++ src/app/mask.directive.ts
@@ -55,52 +55,63 @@ export class MaskDirective {
         this.appMask,
         this._lastMaskedValue.length > val.length,
         this._lastMaskedValue);
+
     if (this.appMask === 'Mm/Dd/YYYY') {
-      let valueArray = val.replace(/\D/g, '');
-      let lenocc = (maskedVal.match(new RegExp("/", "g")) || []).length;
-      let monthOrg = valueArray.substr(0, 2);
-      let month = parseInt(monthOrg);
-      let dateOrg = valueArray.substr(2, 2);
-      let date = parseInt(dateOrg);
-      let yearOrg = valueArray.substr(4, 4);
-      let year = parseInt(yearOrg);
-      let checkDate = 31;
-      if(this.backSpaceState){
-        this.curPosition = this.el.nativeElement.selectionEnd;
-        this.backSpaceState = false;
-      }else{
-        if(valueArray.length === 3 || valueArray.length === 5){
-          this.curPosition = maskedVal.length + 1;
-        }else{
+      if (maskedVal != "") {
+        let valueArray = val.replace(/\D/g, '');
+        let lenocc = (maskedVal.match(new RegExp("/", "g")) || []).length;
+        let monthOrg = valueArray.substr(0, 2);
+        let month = parseInt(monthOrg);
+        let dateOrg = valueArray.substr(2, 2);
+        let date = parseInt(dateOrg);
+        let yearOrg = valueArray.substr(4, 4);
+        let year = parseInt(yearOrg);
+        let checkDate = 31;
+        if (this.backSpaceState) {
           this.curPosition = this.el.nativeElement.selectionEnd;
-        }        
-      }
-      if (month > 12) {
-        return '';
-      } else {
-        if (month === 2) {
-          checkDate = 29;
-        } else if (month === 7) {
-          checkDate = 31;
-        } else if ((month % 2) === 0) {
-          checkDate = 30;
+          this.backSpaceState = false;
+        } else {
+          if (valueArray.length === 3 || valueArray.length === 5) {
+            this.curPosition = maskedVal.length + 1;
+          } else {
+            this.curPosition = this.el.nativeElement.selectionEnd;
+          }
         }
-      }
-      if (date > checkDate) {
-        return monthOrg
-      }
-      if (yearOrg.length === 4 && year < 1900) {
-        return monthOrg + this.seperator + dateOrg;
-      } else {
-        if (month === 2) {
-          let leepyear = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
-          if (yearOrg.length === 4 && !leepyear && date === 29) {
-            return monthOrg;
+        if (month > 12) {
+          this.curPosition = 0;
+          return this.addMaskSlashes('');
+        } else {
+          if (month === 2) {
+            checkDate = 29;
+          } else if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+            checkDate = 31;
+          } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+            checkDate = 30;
+          }
+        }
+        if (date > checkDate) {
+          this.curPosition = 3;
+          return this.addMaskSlashes(monthOrg);
+        }
+        if (yearOrg.length === 4 && year < 1900) {
+          this.curPosition = 6;
+          return this.addMaskSlashes(monthOrg + this.seperator + dateOrg);
+        } else {
+          if (month === 2) {
+            let leepyear = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+            if (yearOrg.length === 4 && !leepyear && date === 29) {
+              this.curPosition = 0;
+              return this.addMaskSlashes(monthOrg);
+            }
           }
         }
       }
+      maskedVal = this.addMaskSlashes(maskedVal);
+      return maskedVal;
+    } else {
+      return maskedVal;
     }
-    return maskedVal;
+
   }
   valueToFormat(value: string, format: string, goingBack = false, prevValue?: string): string {
     let maskedValue = '';
@@ -140,15 +151,50 @@ export class MaskDirective {
         }
       }
     }
-    maskedValue = this.addMaskSlashes(maskedValue);
     return maskedValue;
+  }
+  addMasks(n, dateValue) {
+    let dateValueArray = dateValue.split('');
+    console.log(dateValueArray);
+    let response = [];
+    for (let i = 0; i < n; i++) {
+      if (!isNullOrUndefined(dateValueArray[i])) {
+        response.push(dateValueArray[i]);
+      } else {
+        response.push('_');
+      }
+    }
+    return response.join('');
   }
   addMaskSlashes(maskedValue) {
     if (maskedValue === "") {
-      return "";
+      return "__/__/____";
     }
     if (this.appMask === 'Mm/Dd/YYYY') {
-      //console.log(maskedValue);
+      let dateArray = maskedValue.split(this.seperator);
+      let month = (!isNullOrUndefined(dateArray[0])) ? dateArray[0] : "";
+      let day = (!isNullOrUndefined(dateArray[1])) ? dateArray[1] : "";
+      let year = (!isNullOrUndefined(dateArray[2])) ? dateArray[2] : "";
+      let monthMask = "";
+      let dayMask = "";
+      let yearMask = "";
+      if (month.length === 2) {
+        monthMask = month;
+      } else {
+        monthMask = this.addMasks(2, month);
+      }
+      if (day.length === 2) {
+        dayMask = day;
+      } else {
+        dayMask = this.addMasks(2, day);
+      }
+      if (year.length === 4) {
+        yearMask = year;
+      } else {
+        yearMask = this.addMasks(4, year);
+      }
+      console.log(monthMask + this.seperator + dayMask + this.seperator + yearMask);
+      return monthMask + this.seperator + dayMask + this.seperator + yearMask;
       return maskedValue;
     } else {
       return maskedValue;
